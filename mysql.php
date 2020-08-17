4b9ab92a0fcb5aa443698aa61636b8794d792f3d
Sarbay <sarbay@Sarbay-MacBook-Pro.local>
2020-08-17 15:51:36 +0300

    init

--- mysql.php
+++ mysql.php
@@ -1,2 +1,12 @@
 <?php
-print_r($argv);
+$fileName = $argv[1];
+$contents = file_get_contents($fileName);
+
+$items = explode("# Time:", $contents);
+
+print_r($items);
+echo "\n--------------------\n";
+echo $items[0];
+echo "\n--------------------\n";
+echo $items[1];
+echo "\n--------------------\n";
