6acaa53dcdb3674dd655eb4e4ed4ec1ca4c87529
Sarbay <sarbay@Sarbay-MacBook-Pro.local>
2020-08-17 16:50:24 +0300

    clear output

--- fpmstats.php
+++ fpmstats.php
@@ -3,6 +3,8 @@ exec('sudo service php7.4-fpm status 2>&1', $output);
 $fileName = date("Y-m-d-H-i-s");
 file_put_contents("/home/sarbay/perfmon/fpmstats/7.4.".$fileName.".txt", join("\n", $output));
 
+unset($output);
+
 exec('sudo service php7.1-fpm status 2>&1', $output);
 $fileName = date("Y-m-d-H-i-s");
 file_put_contents("/home/sarbay/perfmon/fpmstats/7.1.".$fileName.".txt", join("\n", $output));
