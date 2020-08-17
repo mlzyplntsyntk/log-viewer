ae7d9ee24904ab8b0eab8df7ff05600c7168e323
Sarbay <sarbay@Sarbay-MacBook-Pro.local>
2020-08-17 15:56:18 +0300

    tt

--- mysql.php
+++ mysql.php
@@ -5,6 +5,15 @@ $contents = file_get_contents($fileName);
 $items = explode("# Time:", $contents);
 
 echo count($items);
+for ($i=0; $i<count($items); $i++) {
+    //ignore first item
+    if ($i == 0) continue;
+
+    $log = explode("\n", $items[$i]);
+    print_r($log);
+    exit;
+}
+
 echo "\n--------------------\n";
 echo $items[0];
 echo "\n--------------------\n";
